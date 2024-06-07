34e45d8e50cf60ef6d1693b31bf0fb0dcfc82135
sebasgfb <sebasgfb1225@gmail.com>
2024-06-06 22:47:00 -0400

    comentarios visibles

--- src/detalleNoticia.js
+++ src/detalleNoticia.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, ActivityIndicator, FlatList, Pressable } from 'react-native';
 import axios from 'axios';
 
 const DetalleNoticia = ({ route }) => {
   const { id } = route.params;
   const [noticia, setNoticia] = useState(null);
   const [listaGrupos, setListaGrupos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [comentariosVisibles, setComentariosVisibles] = useState([]);
+  const [usuarios, setUsuarios] = useState([]);
+  const [sinConexion, setSinConexion] = useState(false);
 
   const obtenerNombreGrupo = (grupoId) => {
     const grupo = listaGrupos.find((grupo) => grupo.id === grupoId);
@@ -18,18 +22,24 @@ const DetalleNoticia = ({ route }) => {
     return date.toLocaleDateString('es-ES', options);
   };
 
+  const obtenerNombreAutor = (autorId) => {
+    const autor = usuarios.find((usuario) => usuario.id === autorId);
+    return autor ? autor.nombre : 'Autor desconocido';
+  };
+
   useEffect(() => {
-    // Realiza la solicitud para obtener los detalles de la noticia usando el ID
     axios
       .get(`http://localhost:8000/listanoticias/${id}`)
       .then((response) => {
         setNoticia(response.data);
+        setLoading(false); 
       })
       .catch((error) => {
         console.error('Error fetching news details:', error);
+        setLoading(false); 
+        setSinConexion(true);
       });
 
-    // Realiza la solicitud para obtener la lista de grupos
     axios
       .get('http://localhost:8000/listagrupos')
       .then((response) => {
@@ -37,15 +47,41 @@ const DetalleNoticia = ({ route }) => {
       })
       .catch((error) => {
         console.error('Error fetching groups:', error);
+        setSinConexion(true);
       });
-  }, []);
 
-  if (!noticia) {
+    axios
+      .get('http://localhost:8000/listacomentarios')
+      .then((response) => {
+        const comentarios = response.data.filter((comentario) => comentario.noticia === id && comentario.visible);
+        setComentariosVisibles(comentarios);
+      })
+      .catch((error) => {
+        console.error('Error fetching comments:', error);
+        setSinConexion(true);
+      });
+
+    axios
+      .get('http://localhost:8000/listausuarios')
+      .then((response) => {
+        setUsuarios(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching users:', error);
+        setSinConexion(true);
+      });
+  }, [id]);
+
+  if (loading) {
     return (
       <View style={styles.container}>
-        <Text>Loading...</Text>
+        <ActivityIndicator size="large" color="#0000ff" />
       </View>
     );
+  }
+
+  if (sinConexion) {
+    return <View style={styles.errorContainer}><Text>Error de conexión</Text></View>;
   }
 
   return (
@@ -56,6 +92,19 @@ const DetalleNoticia = ({ route }) => {
         <Text style={styles.fecha}>{formatDate(noticia.fecha)}</Text>
         <Text style={styles.grupo}>{obtenerNombreGrupo(noticia.grupo)}</Text>
         <Text style={styles.contenido}>{noticia.cuerpo}</Text>
+        <Text style={styles.subtitulo}>Comentarios</Text>
+        <FlatList
+          data={comentariosVisibles}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.tarjeta}>
+              <View style={styles.textoContainer}>
+                <Text style={styles.autor}>{obtenerNombreAutor(item.autor)} - {formatDate(item.fecha)}</Text>
+                <Text style={styles.comentario}>{item.cuerpo}</Text>
+              </View>
+            </View>
+          )}
+        />
       </ScrollView>
     </View>
   );
@@ -64,10 +113,11 @@ const DetalleNoticia = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+  },
+  errorContainer: {
+    flex: 1,
     justifyContent: 'center',
-    padding: 20,
-    backgroundColor: '#fff',
+    alignItems: 'center',
   },
   scrollView: {
     flex: 1,
@@ -98,6 +148,38 @@ const styles = StyleSheet.create({
   contenido: {
     fontSize: 18,
     lineHeight: 24,
+  },
+  subtitulo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  tarjeta: {
+    margin: 8,
+    padding: 10,
+    backgroundColor: "#FFF",
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  textoContainer: {
+    marginLeft: 10,
+  },
+  autor: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginTop: 5,
+  },
+  comentario: {
+    fontSize: 15,
+    lineHeight: 22,
   },
 });
 
